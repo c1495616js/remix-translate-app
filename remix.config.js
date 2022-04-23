ed2134352de941b7e2c4ced263483107ee6e80e8
Jerry Wang <jerryappleid761208@gmail.com>
2022-04-22 23:50:13 -0700

    vercel deploy

--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,13 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['.*'],
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  serverBuildPath: 'build/index.js',
-  publicPath: '/build/',
+  // appDirectory: 'app',
+  // assetsBuildDirectory: 'public/build',
+  // serverBuildPath: 'build/index.js',
+  // publicPath: '/build/',
   // mdx: async (filename) => {
   //   const [rehypeHighlight] = await Promise.all([
   //     import('rehype-highlight').then((mod) => mod.default),
